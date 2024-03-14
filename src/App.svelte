dd2f13a3e6bdcc3e42d9d2e2cf5083e94e3acc79
JinsuRhee <jinsu.rhee@gmail.com>
2024-03-14 11:44:55 +0900

    initialize with frameworks

---
+++ src/App.svelte
@@ -0,0 +1,257 @@
+<script>
+  import "./js/spine";
+  import { getDatabase, ref, onValue } from "firebase/database";
+  import { app } from "./js/db.js";
+
+  $: data = [];
+
+  const db = getDatabase(app);
+  const starCountRef = ref(db, "users");
+  onValue(starCountRef, (snapshot) => {
+    data = snapshot.val();
+    console.log(data);
+  });
+</script>
+
+<div>
+  <!-- Header Navigator -->
+  <header id="nav-container">
+    <a href="index.html"><img src="images/gem_logo.png" /></a>
+    <h1><a href="index.html"> GEM Simulations </a></h1>
+    <nav>
+      <ul>
+        <li><a href="des.html">Descriptions</a></li>
+        <li><a href="spec.html">Spec</a></li>
+        <li><a href="data.html">Data Access</a></li>
+        <li><a href="spine.html">Who does what?</a></li>
+        <li><a href="publication.html">Publications</a></li>
+        <li><a href="index.html">Others</a></li>
+      </ul>
+    </nav>
+  </header>
+
+  <!-- Main page -->
+  <!-- From Here -->
+  <main id="contents-spine">
+    <div>
+      <!-- Usage policy -->
+      <section>
+        <h1>To Use GEM simulation data</h1>
+
+        <p>
+          You are welcome to play with the GEM simulation data <a
+            href="data.html">here</a
+          >. When you are planning to publish your work, please refer to the
+          below publication policy.
+        </p>
+        <br />
+        <ul>
+          <li>Check the conflict of interest of your project</li>
+          <li>Register your project proposal first</li>
+          <li>Wait to be accepted</li>
+          <li>Please update your project progress</li>
+          <li>
+            The core members should be included to the author list
+            <span
+              ><ul class="list_author">
+                <li>Sukyoung Ken Yi (yi@yonsei.ac.kr)</li>
+                <li>San Han (sanhan@yonsei.ac.kr)</li>
+                <li>Jinsu Rhee (jinsu.rhee@gmail.com)</li>
+                <li>J. K. Jang (starbrown816@yonsei.ac.kr)</li>
+                <li>Seyoung Jeon (syj3514@yonsei.ac.kr)</li>
+              </ul></span
+            >
+          </li>
+        </ul>
+      </section>
+      <br />
+
+      <!-- Registration policy -->
+      <section>
+        <h1>To Register your project</h1>
+        <p>
+          You can proceed your research with the GEM simulation data after
+          registering your science project <a href="spine.html"
+            >here (not yet linked)</a
+          >. <!-- update here-->
+        </p>
+
+        <br />
+        <ul>
+          <li>Choose a kind of simulation</li>
+          <li>Describe your science topic</li>
+          <li>Input lead- and co-authors</li>
+          <li>Speicfy the deadline</li>
+        </ul>
+        <br />
+
+        <p>After your proposal is accepted</p>
+        <br />
+        <ul>
+          <li>Keep updating research status</li>
+          <li>Set to "stalled" when your research is stalled</li>
+        </ul>
+      </section>
+      <br />
+
+      <!-- Registration Tool -->
+      <!-- TODO 
+                Submit identifier by user ID
+            -->
+
+      <section>
+        <h1>Register</h1>
+        <p>Submit your science proposal</p>
+        <br />
+        <form id="proposal_form">
+          <fieldset>
+            <table>
+              <colgroup>
+                <col style="width:15%;" />
+                <col style="width:40%;" />
+                <col style="width:12%;" />
+                <col style="width:13%;" />
+                <col style="width:10%;" />
+                <col style="width:20%;" />
+              </colgroup>
+              <tr>
+                <th> Simulation type </th>
+                <th> Topic </th>
+                <th> Lead Author </th>
+                <th> Co-author </th>
+                <th> Email </th>
+                <th> Deadline </th>
+              </tr>
+              <tr>
+                <td style="text-align:left; padding-left:7px; font-size:12pt;">
+                  <p>
+                    <input
+                      class="register_checkbox"
+                      type="checkbox"
+                      id="st_yz"
+                    /> YZiCS
+                  </p>
+                  <br />
+                  <p>
+                    <input
+                      class="register_checkbox"
+                      type="checkbox"
+                      id="st_nh"
+                    /> NewHorizon
+                  </p>
+                  <br />
+                  <p>
+                    <input
+                      class="register_checkbox"
+                      type="checkbox"
+                      id="st_nh2"
+                    /> NewHorizon2
+                  </p>
+                  <br />
+                  <p>
+                    <input
+                      class="register_checkbox"
+                      type="checkbox"
+                      id="st_nc"
+                    /> NewCluster
+                  </p>
+                  <br />
+                </td>
+                <td><input type="text" id="topic" /></td>
+                <td><input type="text" id="lau" /></td>
+                <td><input type="text" id="cau" /></td>
+                <td><input type="text" id="email" /></td>
+                <td><input type="month" id="deadline" /></td>
+              </tr>
+            </table>
+            <br />
+            <div id="buttons" style="top:auto; left:77%;" class="buttons">
+              <input type="submit" value="submit" />
+              <input type="reset" value="reset" />
+            </div>
+          </fieldset>
+        </form>
+      </section>
+      <!-- Why this section does not contain form when it computes height-->
+
+      <!-- Approve / Disapprove the proposed project -->
+      <!-- TODO (only appear to the administrator)-->
+      <section id="approve_project" style="margin-top:170pt;">
+        <h1>Submitted Proposal</h1>
+        <p>This will be shown only to a root user</p>
+        <br />
+
+        <table id="approve_project_list" border="1">
+          <colgroup>
+            <col style="width:13%;" />
+            <col style="width:30%;" />
+            <col style="width:12%;" />
+            <col style="width:13%;" />
+            <col style="width:10%;" />
+            <col style="width:8%;" />
+            <col style="width:14%;" />
+          </colgroup>
+          <tr>
+            <th> Simulation type </th>
+            <th> Topic </th>
+            <th> Lead Author </th>
+            <th> Co-author </th>
+            <th> Email </th>
+            <th> Deadline </th>
+            <th> Decision </th>
+          </tr>
+        </table>
+      </section>
+
+      <!-- Proposal List -->
+      <section style="margin-top:10pt;">
+        <h1>Proposal list</h1>
+
+        <table id="project_list_table">
+          <colgroup>
+            <col style="width:13%;" />
+            <col style="width:30%;" />
+            <col style="width:12%;" />
+            <col style="width:13%;" />
+            <col style="width:10%;" />
+            <col style="width:10%;" />
+            <col style="width:10%;" />
+          </colgroup>
+          <thead>
+            <tr>
+              <th> Simulation Type </th>
+              <th> Topic </th>
+              <th> Lead Author </th>
+              <th> Co-author </th>
+              <th> Email </th>
+              <th> Deadline </th>
+              <th> Status </th>
+            </tr>
+          </thead>
+          <tbody>
+            {#each data as item}
+              <tr>
+                <td> {item.email} </td>
+                <td> {item.username} </td>
+                <td> Lead Author </td>
+                <td> Co-author </td>
+                <td> Email </td>
+                <td> Deadline </td>
+                <td> Status </td>
+              </tr>
+            {/each}
+          </tbody>
+        </table>
+      </section>
+    </div>
+  </main>
+
+  <!-- Footer -->
+  <footer id="footer">
+    <hr />
+    <p>
+      (c) Yonsei University <span class="bold">G</span>alaxy
+      <span class="bold">E</span>volution <span class="bold">M</span>eeting
+    </p>
+  </footer>
+</div>
---
+++ src/js/spine.js
@@ -0,0 +1,159 @@
+import { getDatabase, ref, set } from "firebase/database";
+import { app } from "./db.js";
+
+const db = getDatabase(app);
+
+function writeUserData(userId, name, email, imageUrl) {
+  
+  set(ref(db, 'users/' + userId), {
+    username: name,
+    email: email,
+    profile_picture : imageUrl
+  });
+}
+
+class project{
+    
+    sim_list = ['YZiCS', 'NewHorizon', 'NewHorizon2', 'NewCluster'];
+    sim_type = [false, false, false, false];
+    
+    topic = 'null';
+    lead_author = 'null';
+    co_author = 'null';
+    email = 'null';
+    deadline = 'null';
+};
+
+document.addEventListener('DOMContentLoaded', function() {
+    const form = document.getElementById('proposal_form');
+
+    form.addEventListener('submit', function(event) {
+        event.preventDefault(); // Prevent form submission
+
+        // Get input value by accessing its 'value' property
+        let pj;
+
+        pj = new project();
+
+        pj.topic = document.getElementById('topic').value;
+        pj.lead_author = document.getElementById('lau').value;
+        pj.co_author = document.getElementById('cau').value;
+        pj.email = document.getElementById('email').value;
+        pj.deadline = document.getElementById('deadline').value;
+
+        //pj.sim_type[0] = st_yz = document.getElementById('st_yz').checked;
+        //pj.sim_type[1] = st_yz = document.getElementById('st_nh').checked;
+        //pj.sim_type[2] = st_yz = document.getElementById('st_nh2').checked;
+        //pj.sim_type[3] = st_yz = document.getElementById('st_nc').checked;
+
+        // Display input value in console
+        //console.log('Username:', topic, deadline, st_yz, st_nh, st_nh2, st_nc);
+        console.log('Username:', pj.topic, pj.lead_author, pj.co_author, pj.email, pj.deadline, pj.sim_type[0], pj.sim_type[1], pj.sim_type[2], pj.sim_type[3]);
+        alert('Submitted');
+
+        // Write on DB
+        writeUserData('0', '1', '2', '3');
+
+        make_pjtable_foradmin(pj);
+        // Writeup
+    });
+});
+
+
+function make_pjtable_foradmin(){
+    const table = document.getElementById('approve_project_list');
+    const new_row = table.insertRow();
+    const cell_length = table.rows[0].cells.length;
+
+    console.log('Cell length: ', cell_length);
+
+    for(let i=0; i < cell_length; i++){
+      const new_cell = new_row.insertCell(i);
+      let temp_html = ``;
+      
+      if(i === 0){ // simtype
+        temp_html = `<td>`
+        for(let j=0; j<pj.sim_list.length; j++){
+            if(pj.sim_type[j]){
+                temp_html += pj.sim_list[j] + `<br>`;
+            };
+        };
+        temp_html += `</td>`
+      };
+
+      if(i === 1){ // Topic
+        temp_html = `<td>` + pj.topic + `</td>`; 
+      };
+
+      if(i === 2){ // Lead Author
+        temp_html = `<td>` + pj.lead_author + `</td>`; 
+      };
+
+      if(i === 3){ // Co-author
+        temp_html = `<td>` + pj.co_author + `</td>`; 
+      };
+
+      if(i === 4){ // Email
+        temp_html = `<td>` + pj.email + `</td>`; 
+      };
+
+      if(i === 5){ // Deadline
+        temp_html = `<td>` + pj.deadline + `</td>`; 
+      };
+
+      if(i === 6){ // Decision
+        temp_html = `<td>`;
+        temp_html += `<button onclick="make_pjtable_accept(this)"> Accept </botton>`;
+        temp_html += `<button onclick="make_pjtable_reject(this)"> Reject </botton>`;
+        temp_html += `</td>`;
+      };
+
+      new_cell.insertAdjacentHTML('afterbegin', temp_html);
+    };
+
+    
+   
+    // Adding the entire table to the body tag
+    //doc.appendChild(table);
+    //doc.innerText('Hi~~~');
+    //document.getElementById('approve_project').innerText = '<p>hi~</p>'; // instead of inner text add text?
+};
+
+const make_pjtable_reject = (btn) =>{
+
+    const table = document.getElementById('approve_project_list');
+    const row_index = btn.closest('tr').rowIndex;
+    table.deleteRow(row_index);
+};
+
+const make_pjtable_accept = (btn) =>{
+
+    
+
+    const table = document.getElementById('approve_project_list');
+    const row_index = btn.closest('tr').rowIndex;
+    
+    var row = table.rows[row_index];
+    var tdsNum = row.childElementCount;
+
+    var str = "";
+    
+    var sim_list = row.cells[0].innerHTML;
+    var topic = row.cells[1].innerHTML;
+    var lead_author = row.cells[2].innerHTML;
+    var co_author = row.cells[3].innerHTML;
+    var email = row.cells[4].innerHTML;
+    var deadline = row.cells[5].innerHTML;
+
+    // Write project
+    //var path = '/storage6/GEMHomepage/data/project_list.txt';
+    //var fileobj = new ActiveXObject("Scripting.FileSystemObject");
+    //fWrite = fileobj.CreateTextFile(path);
+    //fWrite.write('aaa');
+    //fWrite.close();
+    //FileWriter fw = new FileWriter(file, true);
+
+    //fw.write('aaa bbb');
+    //fw.flush();
+    //fw.close();
+};
